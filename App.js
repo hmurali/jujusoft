092745faaf00664d3ba5ee37c4277df2da76f3d1
rivendell <webwyler@gmail.com>
2019-11-11 21:47:38 -0600

    add my imports - LineChart, Barchart and sample json files

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import LineChart from './LineChart';
+import BarChart from './BarChart';
+//import DoughnutChart from './DoughnutChart';
+import Chart from 'chart.js';
+import processData from './processData.json';
+import serviceData from './serviceData.json';
