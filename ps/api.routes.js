79057e336596938337c48fd60106d488bed5aa57
Afriocean <onyeyude@gmail.com>
2019-11-13 21:35:44 -0600

    CRUD setup

--- ps/api.routes.js
+++ ps/api.routes.js
@@ -1,64 +1,66 @@
 const router = require("express").Router();
 
 const userservice = {
-    list: function (req, res){
+    list: function (req, res) {
         console.log('get /users')
         res.json({})
-      },
-      create: function (req, res){
-        const body = req.body
-        console.log (body)
-      console.log('get /users')
-      res.json({body})
     },
-
+    create: function (req, res) {
+        const body = req.body
+        console.log(body)
+        console.log('get /users')
+        res.json({ body })
+    },
+    read: function (req, res) {
+        console.log('get /users/:userid', req.params.userid)
+        res.json({})
+    },
+    update: function (req, res) {
+        const body = req.body
+        console.log(body)
+        console.log('put /users/:userid', req.params.userid)
+        res.json({ body })
+    },
+    delete: function (req, res) {
+        console.log('delete /users/:userid', req.params.userid)
+        res.json({})
+    }
 }
 
 //Routes
 router.route('/users')
-    .get (userservice.list)
-    .post (userservice.create)
+    .get(userservice.list)
+    .post(userservice.create)
 
 router.route('/users/:userid')
-    .get(function (req, res){
-       console.log('get /users/:userid', req.params.userid)
-       res.json({})
-    })
-    .put (function (req, res){
-        const body = req.body
-        console.log (body)
-       console.log('put /users/:userid', req.params.userid)
-       res.json({body})
-    })
-    .delete (function (req, res){
-        console.log('delete /users/:userid', req.params.userid)
-        res.json({})
-    })
+    .get(userservice.read)
+    .put(userservice.update)
+    .delete(userservice.delete)
 
 router.route('/computer')
-    .get (function (req, res){
+    .get(function (req, res) {
         console.log('get /computer')
         res.json({})
     })
-    .post (function (req, res){
+    .post(function (req, res) {
         const body = req.body
-        console.log (body)
+        console.log(body)
         console.log('post /computer')
-        res.json({body})
+        res.json({ body })
     })
 
 router.route('/computer/:computerid')
-    .get (function (req, res){
+    .get(function (req, res) {
         console.log('get /computer/:computerid')
         res.json({})
     })
-    .put (function (req, res){
+    .put(function (req, res) {
         const body = req.body
-        console.log (body)
+        console.log(body)
         console.log('put /computer/:computerid')
-        res.json({body})
+        res.json({ body })
     })
-    .delete (function (req, res){
+    .delete(function (req, res) {
         console.log('delete /computer/:computerid')
         res.json({})
     })
