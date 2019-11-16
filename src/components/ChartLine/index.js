179eb96ca4f6728fdcc396d15d42f3fb21844b5a
rivendell <webwyler@gmail.com>
2019-11-16 14:43:41 -0600

    added components folder, ChartLine compononent folder and index.js

---
+++ src/components/ChartLine/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChartLine extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
