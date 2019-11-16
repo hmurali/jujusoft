a8721fee41d07d43421fd3890f98c4c3fe6279ac
rivendell <webwyler@gmail.com>
2019-11-15 23:00:03 -0600

    deleted old js files, moved App.js inside src folder, import processData.json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
-import LineChart from './LineChart';
-import BarChart from './BarChart';
-//import DoughnutChart from './DoughnutChart';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+
 import Chart from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
 import processData from './processData.json';
-import serviceData from './serviceData.json';
+
+//import serviceData from './serviceData.json';
+
+const pData = processData; //array
+//const sData = serviceData; //array - 268 results
+
+console.log("process json: ", pData);
+//console.log("service json: ", sData);
 
 const pData = processData; //object
 const sData = serviceData; //array - 268 results
 
 //Data cleaning
 //delete null values from pData DOES NOT DELETE NULL VALUES!!
-function delet(obj) { 
+function delet(obj) {
   console.log("obj is", obj);
   //console.log("obj.BasePriority is", obj.BasePriority);
-  for (let prop in obj) { 
+  for (let prop in obj) {
     console.log("prop: ", prop);
     console.log("prop value: ", obj[prop]);
-      if (obj.prop === null || obj.prop === undefined) { 
-          delete obj.prop; 
-      }    
-  } 
- return obj; 
+    if (obj.prop === null || obj.prop === undefined) {
+      delete obj.prop;
+    }
+  }
+  return obj;
 }
 
 const processesFiltered = delet(pData);
@@ -32,7 +39,7 @@ const processesFiltered = delet(pData);
 // console.log("pData object is now ", array_pData);
 
 //filter sData for services that can be shut down - 43 results
-const servicesFiltered = sData.filter(function(item) {
+const servicesFiltered = sData.filter(function (item) {
   return item.CanShutdown === true;
 })
 
@@ -44,17 +51,17 @@ console.log("services json Filtered: ", servicesFiltered);
 Chart.defaults.global.defaultFontFamily = "Roboto, sans-serif";
 
 function getData() {
-    let data = [];
-  
-    data.push({
-      title: 'Processes',
-      data: processesFiltered
-    });
-  
-    data.push({
-      title: 'Services',
-      data: servicesFiltered
-    });
+  let data = [];
 
-    return data;
+  data.push({
+    title: 'Processes',
+    data: processesFiltered
+  });
+
+  data.push({
+    title: 'Services',
+    data: servicesFiltered
+  });
+
+  return data;
 }
