2efa3af02755f3e5a07989a58bffb36fdfe366bb
rivendell <webwyler@gmail.com>
2019-11-16 14:52:24 -0600

    updated state

--- src/components/ChartLine/index.js
+++ src/components/ChartLine/index.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import { Line } from 'react-chartjs-2';
+
+
+import processData from '../../processData.json';
+const pData = processData;
 
 export default class ChartLine extends Component {
-    render() {
-        return (
-            <div>
+    constructor(props) {
+        super(props);
 
-            </div>
-        )
+        this.names = []
+        this.values = []
+
+        this.state = {
+            value: "CPU",
+            data: {
+                labels: this.names,
+                datasets: [
+
+                    {
+                        label: "Processes",
+                        backgroundColor: "rgba(0, 255, 0, 0.75)",
+                        data: this.values,
+                    }
+                ]
+
+            }
+        }
+
+        this.aggregateBy = this.aggregateBy.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
     }
+
+
+
+
+
+
 }
