4a0364de9f23fb930bb6e1c481f12549feab9816
rivendell <webwyler@gmail.com>
2019-11-18 21:31:47 -0600

    added JSX components for NavBar, Footer, Jumbotron

--- src/App.js
+++ src/App.js
@@ -18,7 +18,148 @@ class App extends React.Component {
 	}
 
 	render() {
-		return <ChartLine />;
+		<div className="App">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a href="/home">
+					<img src="./img/logo.png" alt="HTML5 Icon" style={{ width: '300px', height: '50px' }} />
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav mr-auto">
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							>
+								Processes
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="signup.html">
+									Processes and Performance
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									CPU
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Memory
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									WiFi
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									GPU
+								</a>
+							</div>
+						</li>
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							>
+								WiFi and Network Data
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="signup.html">
+									DNS History
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Current Network
+								</a>
+							</div>
+						</li>
+
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							>
+								Start or Stop Common Processes
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="signup.html">
+									Chrome
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Visual Studio Code
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Slack
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Custom Script
+								</a>
+
+								<div className="dropdown-divider" />
+								<a className="dropdown-item" href="signup.html">
+									Restart Computer
+								</a>
+								<a className="dropdown-item" href="signup.html">
+									Network
+								</a>
+							</div>
+						</li>
+
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							>
+								Device
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="signup.html">
+									View, Add or Delete Devices
+								</a>
+								{/*
+                        <!-- <a className="dropdown-item" href="signup.html">Home</a> --> */}
+							</div>
+						</li>
+					</ul>
+				</div>
+				<button type="button" className="btn btn-outline-secondary">
+					Sign Up
+				</button>
+				<button type="button" className="btn btn-outline-secondary">
+					Login
+				</button>
+			</nav>
+			<Jumbotron />
+			<ChartLine />
+
+			{/* <WhateverReactUseAsRouter /> */}
+
+			<Footer />
+		</div>;
 	}
 }
 
