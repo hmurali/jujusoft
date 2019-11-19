e3e3c5daa923ac9086e836b9b40631c403644097
Hari <augten95@gmail.com>
2019-11-18 19:34:42 -0600

    Committing loginpage.js and signup.js

---
+++ public/signup.js
@@ -0,0 +1,42 @@
+$(function () {
+
+    // Create new user
+    $(`#create-form`).on(`submit`, function (event) {
+
+        event.preventDefault();
+
+        var newUser = {
+
+            firstName: $(`#new-user-firstName`).val().trim(),
+            lastName: $(`#new-user-lastName`).val().trim(),
+            email: $(`#new-user-email`).val().trim(),
+            password: $(`#new-user-password`).val().trim()
+
+        }
+
+        console.log(newUser);
+
+        $.ajax(`/api/users`, {
+            type: `POST`,
+            data: newUser
+        }).then((user) => {
+            loginUser(newUser.email, newUser.password);
+        });
+
+    });
+
+
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members/dashboard page
+    function loginUser(email, password) {
+        $.post("/api/loginpage", {
+            email: email,
+            password: password
+        }).then(function (data) {
+            window.location.replace(data);
+            // If there's an error, log the error
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+});
