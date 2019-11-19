ffdfb8fa8a4b79923bfc0c50098c24670f8cc681
rivendell <webwyler@gmail.com>
2019-11-18 20:13:22 -0600

    added Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import '../css/style.css'
+
+export default function Footer() {
+	return (
+		<div>
+			<footer id="sticky-footer" className="py-4 bg-light text-blue-50" data-component="Footer" />
+			<div className="container text-center">
+				<small>Copyright 2019 &copy; JujuSoft</small>
+			</div>
+			<footer />
+		</div>
+	);
+}
