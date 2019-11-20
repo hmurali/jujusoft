eff8c5ef3da7e4ba056fe5e8c82271571d2f2757
rivendell <webwyler@gmail.com>
2019-11-19 23:34:01 -0600

    modified Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,11 @@ import React from 'react';
 export default function Footer() {
 	return (
 		<div>
-			<footer id="sticky-footer" className="py-4 bg-light text-blue-50" data-component="Footer" />
-			<div className="container text-center">
-				<small>Copyright 2019 &copy; JujuSoft</small>
-			</div>
-			<footer />
+			<footer id="sticky-footer" className="py-4 bg-light text-blue-50" data-component="Footer">
+				<div className="container text-center">
+					<small>Copyright 2019 &copy; JujuSoft</small>
+				</div>
+			</footer>
 		</div>
 	);
 }
